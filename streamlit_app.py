4cb9e4fa1cf75cc37859645a9eedcad7cc0da920
eduardIOC <eduardo.latorre@gmail.com>
2023-11-17 12:41:56 +0100

    Update streamlit_app.py text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,10 @@
 import streamlit
-streamlit.title('Hello World')
+streamlit.title('My parents new Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🐔Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥑Kale, Spinach & Rocket Smoothie')
+streamlit.text('🍞Hard-Boiled Free-Range Egg')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
