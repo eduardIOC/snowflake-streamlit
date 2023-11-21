9252433e04b81ae7917f16123f6e0ba7e041668f
eduardIOC <eduardo.latorre@gmail.com>
2023-11-21 15:05:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ def get_fruityvice_data(fruit_choice):
     return fruityvice_normalized
 
 def get_fruit_load_list():
-    with ny_cnx.cursor() as my_cur:
+    with my_cnx.cursor() as my_cur:
         my_cur.execute("SELECT * from fruit_load_list")
     return my_cur.fetchall()
 
